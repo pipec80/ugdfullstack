823607efa900327fdd3e01cf59e9b2c418dcf494
felipe castro valenzuela <felipe castro valenzuela>
2017-06-08 15:22:30 -0400

    ajuste view

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,6 +3,7 @@ var mainApp = angular.module('home.controllers', []);
 mainApp.controller('mainController', ['$scope', 'dataResource', function($scope, dataResource) {
         $scope.titulo = "Puntos de recarga BIP! en estaciones Metro";
         $scope.listado = {};
+        $scope.mostrar = false;
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 console.log(position.coords.Coordinates, 'current position');
@@ -10,22 +11,23 @@ mainApp.controller('mainController', ['$scope', 'dataResource', function($scope,
                     $scope.position = '';
                     $scope.latitude = position.coords.latitude;
                     $scope.longitude = position.coords.longitude;
-                    $scope.position = $scope.longitude + '--' + $scope.latitude;
+                    $scope.position = $scope.longitude + '|--|' + $scope.latitude;
 
-                    dataResource.query().$promise.then(function(data) {
+                    dataResource.query({ limit: 2, resource_id: 'ba0cd493-8bec-4806-91b5-4c2b5261f65e' }).$promise.then(function(data) {
                         $scope.listado = data.result.records;
                         $scope.totalItems = $scope.listado.length;
                         console.log('result: ', data.result.records);
+
                     }, function(errResponse) {
                         console.log('Failed: ', errResponse);
                     });
-
+                    $scope.mostrar = true;
                 });
             });
         }
     }])
     .factory('dataResource', ['$resource', function($resource) {
-        return $resource('http://datos.gob.cl/api/action/datastore_search?resource_id=ba0cd493-8bec-4806-91b5-4c2b5261f65e&limit=5', //la url donde queremos consumir
+        return $resource('http://datos.gob.cl/api/action/datastore_search', //la url donde queremos consumir
             {}, //aquí podemos pasar variables que queramos pasar a la consulta
             //a la función get le decimos el método, y, si es un array lo que devuelve
             //ponemos isArray en true
